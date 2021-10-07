58e14bbfcd034e4e8eaa70861565160661f3357a
simonabras <91312929+simonabras@users.noreply.github.com>
2021-10-07 15:32:22 +0200

    algo - table

---
+++ Algo - Table de 5/Algo - Table de 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algo___Table_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string table = "0";
+
+            while(number < 500)
+            {
+                number = number + 5;
+                table = table + ", " + number;
+            }
+
+            Console.WriteLine(table);
+        }
+    }
+}
+
